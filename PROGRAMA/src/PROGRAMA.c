62b4b2f435fe558f87c76cb78b7797ba26656958
vilarod <utnso@utnso.(none)>
2014-04-26 16:43:07 -0300

    subo programa con commons

--- PROGRAMA/src/PROGRAMA.c
+++ PROGRAMA/src/PROGRAMA.c
@@ -10,8 +10,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
+//Ruta del config
+#define PATH_CONFIG "/home/utnso/tp-2014-1c-garras/UMV/src/config.cfg"
+
 
 int main(void) {
+
+	int tamanioMemoria = ObtenerTamanioMemoriaConfig();
 	puts("!!!Hello World!!!");
 	return EXIT_SUCCESS;
+}
+int ObtenerTamanioMemoriaConfig()
+{
+	t_config* config = config_create(PATH_CONFIG);
+
+	return config_get_int_value(config, "TAMANIO_MEMORIA");
 }
---
+++ PROGRAMA/src/PROGRAMA.h
@@ -0,0 +1,13 @@
+/*
+ * PROGRAMA.h
+ *
+ *  Created on: 26/04/2014
+ *      Author: utnso
+ */
+
+#ifndef PROGRAMA_H_
+#define PROGRAMA_H_
+
+int ObtenerTamanioMemoriaConfig();
+
+#endif /* PROGRAMA_H_ */
