7b8b7c625231e68011f92d46c8a094a1db9af75b
Romina Maldonado <romi.mld.utn@gmail.com>
2014-05-02 19:23:59 -0300

    empezando con las primitivas

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -22,8 +22,6 @@
 #include <sys/wait.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
-//includes para sockets
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -63,19 +61,6 @@ void ConexionConSocket()
 
 	   puts("Entre a conexionConSocket!");
 
-	}
-
-
-
-
-
-int main(void) {
-
-
-	UMV_PUERTO = ObtenerPuertoUMV();
-	printf("El puerto de la memoria es: %d ", UMV_PUERTO);
-	ConexionConSocket();
-		return EXIT_SUCCESS;
 
 	}
 
@@ -123,3 +108,183 @@ int ObtenerPuertoUMV()
 
 
 
+
+int main(void) {
+
+
+	UMV_PUERTO = ObtenerPuertoUMV();
+	printf("El puerto de la memoria es: %d ", UMV_PUERTO);
+	ConexionConSocket();
+
+		return EXIT_SUCCESS;
+
+	}
+
+
+
+
+
+// Primitivas
+//esto es solamente un bosquejo para ir armandolas
+
+
+void asignar(t_puntero direccion_variable, t_valor_variable valor)
+{
+	//direccion_variable=valor;
+}
+
+t_valor_variable obtenerValorCompartida(t_nombre_compartida variable)
+{
+	t_valor_variable valor;
+	valor=1; //inicializo de prueba
+
+	//Acá tengo que obtener el valor de la variable mediante la sys call al kernel
+	// obtener_valor(variable);
+
+	return valor; //devuelve el valor de la variable
+}
+
+t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor)
+{
+	t_valor_variable valor_asig;
+		valor_asig=1; //inicializo de prueba
+
+	//Acá también uso sys call a kernel
+	// grabar_valor(variable,valor);
+
+	return valor_asig; //devuelve el valor asignado
+}
+
+t_puntero_instruccion llamarSinRetorno(t_nombre_etiqueta etiqueta, t_puntero_instruccion linea_en_ejecucion)
+{
+	t_puntero_instruccion instr;
+     instr=1; //inicializo de prueba
+
+	//preservar el contexto de ejecucion actual y resetear estructuras. necesito un contexto vacio ahora
+
+	return instr;
+}
+
+
+t_puntero_instruccion llamarConRetorno(t_nombre_etiqueta etiqueta,
+					  t_puntero donde_retornar,
+		              t_puntero_instruccion linea_en_ejecucion)
+{
+	t_puntero_instruccion instr;
+	instr=1; //inicializo de prueba
+
+	//preservar el contexto actual para retornar al mismo
+
+	return instr;
+}
+
+t_puntero_instruccion finalizar(void)
+{
+	t_puntero_instruccion instr;
+	instr=1; //inicializo de prueba
+	//recuperar pc y contexto apilados en stack.Si finalizo, devolver -1
+
+	return instr;
+}
+
+t_puntero_instruccion retornar (t_valor_variable retorno)
+{
+	t_puntero_instruccion instr;
+	instr=1; //inicializo de prueba
+	//acá tengo que volver a retorno
+
+	return instr;
+}
+
+int imprimir (t_valor_variable valor_mostrar)
+{
+	int cant_caracteres;
+	cant_caracteres=1; //inicializo de prueba
+
+	//acá me conecto con el kernel y le paso el mensaje
+
+	return cant_caracteres;
+}
+
+t_valor_variable dereferenciar(t_puntero direccion_variable)
+{
+	t_valor_variable valor;
+	valor=1;//inicializo de prueba
+
+	//tiene que leer el valor que se guarda en esa posición
+
+	return valor; //devuelve el valor encontrado
+}
+
+t_puntero_instruccion irAlLabel(t_nombre_etiqueta etiqueta)
+{
+	t_puntero_instruccion primer_instr;
+	primer_instr=1;
+	//busco la primer instruccion ejecutable
+
+	return primer_instr; //devuelve la instruccion encontrada
+}
+
+t_puntero obtenerPosicionVariable(t_nombre_variable identificador_variable)
+{
+	t_puntero posicion;
+	posicion=1; //inicializo de prueba
+	//busco la posicion de la variable
+
+	return posicion; //devuelvo la posicion
+}
+
+t_puntero definirVariable(t_nombre_variable identificador_variable)
+{
+	t_puntero pos_var_stack;
+	pos_var_stack=1; //inicializo de prueba
+	// reserva espacio para la variable,
+	//la registra en el stack y en el diccionario de variables
+
+	return pos_var_stack; //devuelvo la pos en el stack
+}
+
+int imprimirTexto(char* texto)
+{
+	int cant_caracteres;
+		cant_caracteres=1; //inicializo de prueba
+
+		//acá me conecto con el kernel y le paso el mensaje
+
+		return cant_caracteres;
+}
+
+int entradaSalida(t_nombre_dispositivo dispositivo, int tiempo)
+{
+	int tiempo2;
+	tiempo2=1; //inicializo de prueba
+
+	//acá pido por sys call a kernel
+	// entrada_salida(dispositivo,tiempo);
+
+	return tiempo2;
+}
+
+/*
+int wait(t_nombre_semaforo identificador_semaforo)
+{
+	int bloquea;
+	bloquea=0;//incializo de prueba
+
+	//sys call con kernel
+	// wait(identificador_semaforo);
+
+	return bloquea;
+}
+
+int signal(t_nombre_semaforo identificador_semaforo)
+{
+	int desbloquea;
+	desbloquea=1; //inicializo de prueba
+
+	//sys call con kernel
+	// signal(identificador_semaforo);
+
+	return desbloquea;
+}
+*/
--- CPU/src/CPU.h
+++ CPU/src/CPU.h
@@ -7,11 +7,37 @@
 
 #ifndef CPU_H_
 #define CPU_H_
+#include<parser/parser.h>
 
 int ObtenerPuertoUMV();
 char* ObtenerIPUMV();
+int Enviar (int sRemoto, void * buffer);
 void ConexionConSocket();
 void Cerrar();
+
+
+//Primitivas
+
+void asignar(t_puntero dir, t_valor_variable val);
+t_valor_variable obtenerValorCompartida(t_nombre_compartida var);
+t_valor_variable asignarValorCompartida(t_nombre_compartida var, t_valor_variable val);
+t_puntero_instruccion llamarSinRetorno(t_nombre_etiqueta etiq, t_puntero_instruccion lineaEjc);
+t_puntero_instruccion llamarConRetorno(t_nombre_etiqueta etiq,
+					  t_puntero donde_ret,
+		              t_puntero_instruccion linea_ejec);
+t_puntero_instruccion finalizar(void);
+t_puntero_instruccion retornar (t_valor_variable ret);
+int imprimir (t_valor_variable valor_mostrar);
+t_valor_variable dereferenciar(t_puntero dir_var);
+t_puntero_instruccion irAlLabel(t_nombre_etiqueta etiq);
+t_puntero obtenerPosicionVariable(t_nombre_variable id_var);
+t_puntero definirVariable(t_nombre_variable id_var);
+int imprimirTexto(char* texto);
+int entradaSalida(t_nombre_dispositivo disp, int tiempo);
+/* int wait(t_nombre_semaforo id_sem);
+int signal(t_nombre_semaforo id_sem); */
+
+
 
 #endif /* CPU_H_ */
 
