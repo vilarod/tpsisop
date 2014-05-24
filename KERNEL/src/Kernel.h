436f2f95043d1fc4a891a634fe54c7d25b5a8173
Ignacio Lascurain <lascurainignacio@gmail.com>
2014-05-24 12:43:33 -0300

    sockets funcionando! wiiiiiiiii

--- KERNEL/src/Kernel.h
+++ KERNEL/src/Kernel.h
@@ -1,6 +1,7 @@
 void ConexionConSocket();
+int Recibir (int sRemoto, char * buffer);
 int ObtenerPuertoUMV();
-int Enviar (int sRemoto, void * buffer);
+int Enviar (int sRemoto, char * buffer);
 void Cerrar (int sRemoto);
 void *PLP(void *arg);
 void *PCP(void *arg);
