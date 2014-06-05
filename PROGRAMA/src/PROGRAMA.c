ed0a6226b059d1133f54566cffb7dbd21f70137b
vilarod <utnso@utnso.(none)>
2014-06-05 11:54:12 -0300

    cambios

--- PROGRAMA/src/PROGRAMA.c
+++ PROGRAMA/src/PROGRAMA.c
@@ -18,7 +18,9 @@
 #include <netdb.h>
 #include <string.h>
 #include "PROGRAMA.h"
-
+#include <unistd.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
 //#include <sys/stat.h>
 //#include <fcntl.h>
 
@@ -28,10 +30,11 @@
 //Tipo de servidor conectado
 //#define  TIPO_KERNEL       1
 
-
 //Mensajes aceptados
 #define HANDSHAKE '31'
 #define ENVIARPROGRAMA '11'
+//Tamaño buffer
+#define BUFFERSIZE 1024
 
 //Puerto
 //#define IP "127.0.0.1"
@@ -40,111 +43,124 @@
 #define MAXLONG 1024
 int ObtenerPuertoKernel();
 char* ObtenerIpKernel();
-
-
-int main(int argc, char* argv[])
-{
-
-
-    int index;    //para parametros
-
-    for(index = 0; index < argc; index++)//parametros
-     	printf("  Parametro %d: %s\n", index, argv[index]);
-
-    //argv[0] es el path: /home/utnso/tp-2014/1c-garras/PROGRAMA/Debug/PROGRAM/
-    //argv[1] es el nombre del programa
-
-    //se creo el directorio ansisop en /usr/bin con sudo mkdir ansisop
-    //se uso sudo para poder ejecutar comandos que requieren permisos de administrador
-    /*El symbolic link se hizo por consola:
-      	  sudo ln -s /home/utnso/tp-2014-1c-garras/PROGRAMA/Debug/PROGRAMA /usr/bin/ansisop*/
+int ImprimirTrazaPorConsola = 1; //si es cero no imprime la traza
+void Traza(const char* mensaje, ...);
+void Error(const char* mensaje, ...);
+void ErrorFatal(char mensaje[], ...);
+void CerrarSocket(int socket);
+int EnviarDatos(int socket, void *buffer);
+int RecibirDatos(int socket, char *buffer);
+int ConexionConSocket(int puerto, char* IP);
+int analizarRespuestaKERNEL(char *mensaje);
+int hacerhandshakeKERNEL(int sockfd);
+char* ObtenerIpKernel();
+void conectarAKERNEL();
+int soquete;
 
 
 
 
-    FILE* f;
-    char* contents;
-    size_t len;
-    size_t bytesRead;
+int main(int argc, char* argv[]) {
 
-    f = fopen(argv[1], "r");//abre el archivo en modo read
-    if (f == NULL) {
-    	fprintf(stderr, "Error opening file: %s", argv[1]);//No existe el archivo
-    	return 1;
-     }
-    fseek(f, 0, SEEK_END);//nos situamos al final del archivo
-    len = ftell(f);//nos da la cantidad de bytes del archivo
-    rewind(f);
+	int index;    //para parametros
 
+	for (index = 0; index < argc; index++)    //parametros
+		printf("  Parametro %d: %s\n", index, argv[index]);
 
-    contents = (char*) malloc(sizeof(char) * len + 1);//leer lo que contiene
-    contents[len] = '\0'; // solo es necesario para imprimir la salida con printf
-    if (contents == NULL) {
-    	fprintf(stderr, "Failed to allocate memory");//imprime error sino tiene memoria
-    return 2;
-    }
+	//argv[0] es el path: /home/utnso/tp-2014/1c-garras/PROGRAMA/Debug/PROGRAM/
+	//argv[1] es el nombre del programa
 
-    bytesRead = fread(contents, sizeof(char), len, f);
+	//se creo el directorio ansisop en /usr/bin con sudo mkdir ansisop
+	//se uso sudo para poder ejecutar comandos que requieren permisos de administrador
+	/*El symbolic link se hizo por consola:
+	 sudo ln -s /home/utnso/tp-2014-1c-garras/PROGRAMA/Debug/PROGRAMA /usr/bin/ansisop*/
 
-    txt_close_file(f); //cierra el archivo
+	FILE* f;
+	char* contents;
+	size_t len;
+	size_t bytesRead;
 
+	f = fopen(argv[1], "r");    //abre el archivo en modo read
+	if (f == NULL ) {
+		fprintf(stderr, "Error opening file: %s", argv[1]); //No existe el archivo
+		return 1;
+	}
+	fseek(f, 0, SEEK_END);    //nos situamos al final del archivo
+	len = ftell(f);    //nos da la cantidad de bytes del archivo
+	rewind(f);
 
-    printf("File length: %d, bytes read: %d\n", len, bytesRead);//imprime la cantidad de bytes del archivo
-    printf("Contents:%s",contents);
+	contents = (char*) malloc(sizeof(char) * len + 1);    //leer lo que contiene
+	contents[len] = '\0'; // solo es necesario para imprimir la salida con printf
+	if (contents == NULL ) {
+		fprintf(stderr, "Failed to allocate memory"); //imprime error sino tiene memoria
+		return 2;
+	}
 
-    char **linea;
-    char *separator;
-    char* nuevo=(char*)malloc(len*sizeof(char) + 1);//aca guardo el programa como lo recibe el kernel
-    strcpy(nuevo, "");
-    separator = "\n";
-    linea = string_split(contents, separator);//separa el programa ansisop en lineas
-    int i;
+	bytesRead = fread(contents, sizeof(char), len, f);
 
-    for(i=1; linea[i]!= NULL; i++)//elimino la primer linea del shebang
-    {
-    	//printf("%s",linea[i]);
+	txt_close_file(f); //cierra el archivo
 
-    	string_append(&nuevo,linea[i]);//concatena todas las lineas del programa
+	printf("File length: %d, bytes read: %d\n", len, bytesRead); //imprime la cantidad de bytes del archivo
+	printf("Contents:%s", contents);
 
-    }
-    printf("\n");
-    printf("%s", nuevo);//verifico que tengo el programa sin la primer linea
-    printf("\n");
+	char **linea;
+	char *separator;
+	char* nuevo = (char*) malloc(len * sizeof(char) + 1); //aca guardo el programa como lo recibe el kernel
+	strcpy(nuevo, "");
+	separator = "\n";
+	linea = string_split(contents, separator); //separa el programa ansisop en lineas
+	int i;
 
-    free(contents);
-    free(nuevo);
+	for (i = 1; linea[i] != NULL ; i++) //elimino la primer linea del shebang
+			{
+		//printf("%s",linea[i]);
 
+		string_append(&nuevo, linea[i]); //concatena todas las lineas del programa
 
-        //ConexionConSocket();
+	}
+	printf("\n");
+	printf("%s", nuevo); //verifico que tengo el programa sin la primer linea
+	printf("\n");
 
-        return 0;
+	free(contents);
+	free(nuevo);
+
+	conectarAKERNEL();
+
+	return 0;
 }
 
-
-int ObtenerPuertoKernel()
-{
+int ObtenerPuertoKernel() {
 	t_config* config = config_create(PATH_CONFIG);
 
 	return config_get_int_value(config, "PUERTO_KERNEL");
 
 }
 
-char* ObtenerIpKernel()
-{
+char* ObtenerIpKernel() {
 	t_config* config = config_create(PATH_CONFIG);
 
-	return config_get_int_value(config, "IP_KERNEL");
+	return config_get_string_value(config, "IP_KERNEL");
 
 }
 
-void conectarAKERNEL(){
-		Traza("Intentando conectar a umv.");
-		socket = ConexionConSocket(7000, ObtenerIpKernel());//el puerto es de la umv para probar
-		if (hacerhandshakeKERNEL(socket) == 0) {
-			ErrorFatal("No se pudo conectar al kernel");
-		}
+void conectarAKERNEL() {
+	Traza("Intentando conectar a umv.");
+	soquete = ConexionConSocket(7000, "10.5.5.111"); //el puerto es de la umv para probar
+	if (hacerhandshakeKERNEL(soquete) == 0) {//donde esta la ip va obtenerIpKernel()
+		ErrorFatal("No se pudo conectar al kernel");
+	}
 }
-	
+int hacerhandshakeKERNEL(int sockfd) {
+	char respuestahandshake[BUFFERSIZE];
+	char *mensaje = "31";
+
+	EnviarDatos(sockfd, mensaje);
+	RecibirDatos(sockfd, respuestahandshake);
+	return analizarRespuestaKERNEL(respuestahandshake);
+
+}
+
 int analizarRespuestaKERNEL(char *mensaje) {
 	if (mensaje[0] == 0) {
 		Error("eL KERNEL nos devolvio un error: %s", mensaje);
@@ -152,9 +168,119 @@ int analizarRespuestaKERNEL(char *mensaje) {
 	} else
 		return 1;
 }
+int ConexionConSocket(int puerto, char* IP) {//crea el socket y me retorna el int
+	int sockfd;
+	//Ip de lo que quieres enviar: ifconfig desde terminator , INADDR_ANY para local
+	struct hostent *he, *gethostbyname();
+	struct sockaddr_in their_addr;
+	he = gethostbyname(IP);
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		ErrorFatal("Error al querer crear el socket. puerto %d, ip %s", puerto,
+				IP);
+		exit(1);
+	}
+
+	their_addr.sin_family = AF_INET; // Ordenación de bytes de la máquina
+	their_addr.sin_port = htons(puerto); // short, Ordenación de bytes de la red
+	bcopy(he->h_addr, &(their_addr.sin_addr.s_addr),he->h_length); //their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+	memset(&(their_addr.sin_zero), '\0', 8); // poner a cero el resto de la estructura
+
+	if (connect(sockfd, (struct sockaddr *) &their_addr,
+			sizeof(struct sockaddr)) == -1) {
+		ErrorFatal("Error al querer conectar. puerto %d, ip %s", puerto, IP);
+		exit(1);
+	}
+
+	return sockfd;
+}
+
+int RecibirDatos(int socket, char *buffer) {
+	int bytecount;
+// memset se usa para llenar el buffer con 0s
+	memset(buffer, 0, BUFFERSIZE);
+
+//Nos ponemos a la escucha de las peticiones que nos envie el cliente. //aca si recibo 0 bytes es que se desconecto el otro, cerrar el hilo.
+	if ((bytecount = recv(socket, buffer, BUFFERSIZE, 0)) == -1)
+		Error(
+				"Ocurrio un error al intentar recibir datos desde uno de los clientes. Socket: %d",
+				socket);
+
+	Traza("RECIBO datos. socket: %d. buffer: %s", socket, (char*) buffer);
+
+	return bytecount;
+}
+
+int EnviarDatos(int socket, void *buffer) {
+	int bytecount;
+
+	if ((bytecount = send(socket, buffer, strlen(buffer), 0)) == -1)
+		Error("No puedo enviar información a al clientes. Socket: %d", socket);
+
+	Traza("ENVIO datos. socket: %d. buffer: %s", socket, (char*) buffer);
+
+	return bytecount;
+}
+
+void error(int code, char *err) {
+	char *msg = (char*) malloc(strlen(err) + 14);
+	sprintf(msg, "Error %d: %s\n", code, err);
+	fprintf(stderr, "%s", msg);
+	exit(1);
+}
+void Cerrar(int sRemoto) {
+
+	close(sRemoto);
+}
+void CerrarSocket(int socket) {
+	close(socket);
+	Traza("Se cerró el socket (%d).", socket);
+}
+void ErrorFatal(char mensaje[], ...) {
+	char* nuevo;
+	va_list arguments;
+	va_start(arguments, mensaje);
+	nuevo = string_from_vformat(mensaje, arguments);
+
+	fprintf(stderr, "\nERROR FATAL: %s\n", nuevo);
+
+	va_end(arguments);
+
+	char fin;
+
+	printf(
+			"El programa se cerrara. Presione ENTER para finalizar la ejecución.");
+	scanf("%c", &fin);
+
+	exit(EXIT_FAILURE);
+}
+void Error(const char* mensaje, ...) {
+	char* nuevo;
+	va_list arguments;
+	va_start(arguments, mensaje);
+	nuevo = string_from_vformat(mensaje, arguments);
+
+	fprintf(stderr, "\nERROR: %s\n", nuevo);
+
+	va_end(arguments);
+
+}
+void Traza(const char* mensaje, ...) {
+	if (ImprimirTrazaPorConsola) {
+		char* nuevo;
+		va_list arguments;
+		va_start(arguments, mensaje);
+		nuevo = string_from_vformat(mensaje, arguments);
+
+		printf("TRAZA--> %s \n", nuevo);
+
+		va_end(arguments);
+
+	}
+}
 
 void txt_close_file(FILE* file) {
 
-   	fclose(file);
+	fclose(file);
 }
 
