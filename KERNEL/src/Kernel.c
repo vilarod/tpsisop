65a94dac6e760f8496257df3105eaedb958bc600
Ignacio Lascurain <lascurainignacio@gmail.com>
2014-06-14 12:49:22 -0300

    avanzo select

--- KERNEL/src/Kernel.c
+++ KERNEL/src/Kernel.c
@@ -227,8 +227,104 @@ void *moverReady(void *arg) {
 }
 
 void crearEscucha() {
-	//while(1);
-	//Crear select
+	fd_set master;   // conjunto maestro de descriptores de fichero
+	fd_set read_fds; // conjunto temporal de descriptores de fichero para select()
+	struct sockaddr_in myaddr;     // dirección del servidor
+	struct sockaddr_in remoteaddr; // dirección del cliente
+	int fdmax;        // número máximo de descriptores de fichero
+	int listener;     // descriptor de socket a la escucha
+	int newfd;        // descriptor de socket de nueva conexión aceptada
+	char buf[BUFFERSIZE];    // buffer para datos del cliente
+	int nbytes;
+	int yes = 1;        // para setsockopt() SO_REUSEADDR, más abajo
+	int addrlen;
+	int i, j;
+	FD_ZERO(&master);    // borra los conjuntos maestro y temporal
+	FD_ZERO(&read_fds);
+	// obtener socket a la escucha
+	if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("error al crear socket");
+		exit(1);
+	}
+	// obviar el mensaje "address already in use" (la dirección ya se está usando)
+	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
+			== -1) {
+		perror("setsockopt, la direccion ya se esta usando");
+		exit(1);
+	}
+	// enlazar
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_addr.s_addr = INADDR_ANY;
+	myaddr.sin_port = htons(Puerto);
+	memset(&(myaddr.sin_zero), '\0', 8);
+	if (bind(listener, (struct sockaddr *) &myaddr, sizeof(myaddr)) == -1) {
+		perror("error en el bind");
+		exit(1);
+	}
+	// escuchar
+	if (listen(listener, 10) == -1) {
+		perror("error en el listen");
+		exit(1);
+	}
+	// añadir listener al conjunto maestro
+	FD_SET(listener, &master);
+	// seguir la pista del descriptor de fichero mayor
+	fdmax = listener; // por ahora es éste
+	// bucle principal
+	for (;;) {
+		read_fds = master; // cópialo
+		if (select(fdmax + 1, &read_fds, NULL, NULL, NULL ) == -1) {
+			perror("select");
+			exit(1);
+		}
+		// explorar conexiones existentes en busca de datos que leer
+		for (i = 0; i <= fdmax; i++) {
+			if (FD_ISSET(i, &read_fds)) { // ¡¡tenemos datos!!
+				if (i == listener) {
+					// gestionar nuevas conexiones
+					addrlen = sizeof(remoteaddr);
+					if ((newfd = accept(listener,
+							(struct sockaddr *) &remoteaddr, &addrlen)) == -1) {
+						perror("accept");
+					} else {
+						FD_SET(newfd, &master); // añadir al conjunto maestro
+						if (newfd > fdmax) {    // actualizar el máximo
+							fdmax = newfd;
+						}
+						printf("selectserver: new connection from %s on "
+								"socket %d\n", inet_ntoa(remoteaddr.sin_addr),
+								newfd);
+					}
+				} else {
+					// gestionar datos de un cliente
+					if ((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0) {
+						// error o conexión cerrada por el cliente
+						if (nbytes == 0) {
+							// conexión cerrada
+							printf("selectserver: socket %d hung up\n", i);
+						} else {
+							perror("recv");
+						}
+						close(i); // bye!
+						FD_CLR(i, &master); // eliminar del conjunto maestro
+					} else {
+						// tenemos datos de algún cliente
+						for (j = 0; j <= fdmax; j++) {
+							// ¡enviar a todoos el mundo!
+							if (FD_ISSET(j, &master)) {
+								// excepto al listener y a nosotros mismos
+								if (j != listener && j != i) {
+									if (send(j, buf, nbytes, 0) == -1) {
+										perror("send");
+									}
+								}
+							}
+						}
+					}
+				} // Esto es ¡TAN FEO! demasiado feo
+			}
+		}
+	}
 
 	int AtiendeCliente(int sockete); //Handshake y recibir programa
 
@@ -444,14 +540,21 @@ int chartToInt(char x) {
 }
 
 void ComandoRecibirPrograma(char *buffer, int *idProg, int *tipoCliente) {
-	(*idProg) = chartToInt(buffer[1]);
-	(*tipoCliente) = chartToInt(buffer[2]);
-
 	memset(buffer, 0, BUFFERSIZE);
+	PCB programa;
+	programa.id = id;
+	programa.programCounter= 0; // o 1
+	programa.segmentoCodigo;
+	programa.segmentoStack;
+	programa.cursorStack;
+	//int indiceCodigo;
+	//int indiceEtiquetas;
+	//int programCounter;
+	//int sizeContextoActual;
+	//int sizeIndiceEtiquetas;
 
-	//Recibir programa
-	//CrearPCB();
-	if (pedirMemoriaUMV(socketumv)) {
+
+	if (pedirMemoriaUMV(socketumv,programa)) {
 		//Responder programa too ok
 	} else {
 		//responder programa too mal
@@ -497,7 +600,7 @@ int AtiendeCliente(int sockete) {
 			EnviarDatos(sockete, "1");
 			break;
 		case MSJ_RECIBO_PROGRAMA:
-			ComandoRecibirPrograma(buffer, &id_Programa, &tipo_Conexion);
+			ComandoRecibirPrograma(buffer, sockete);
 			EnviarDatos(sockete, "1");
 		}
 	}
