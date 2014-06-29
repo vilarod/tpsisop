8b80640b8fe995bdb8c7c5eed57cd20ee80d7310
vilarod <vilarod@gmail.com>
2014-06-28 21:46:26 -0300

    saco cunit

--- PROGRAMA/src/PROGRAMA.c
+++ PROGRAMA/src/PROGRAMA.c
@@ -366,25 +366,3 @@ void traza(const char* mensaje, ...) {
 
 	}
 }
-int correrTests() {
-	CU_initialize_registry();
-	void test1() {
-		printf("Soy el test 1!, y pruebo que 2 sea igual a 1+1");
-		CU_ASSERT_EQUAL(1 + 1, 2);
-	}
-
-	void test2() {
-		printf("Soy el test 2!");
-	}
-
-	CU_pSuite prueba = CU_add_suite("Suite de prueba", NULL, NULL );
-	CU_add_test(prueba, "uno", test1);
-	CU_add_test(prueba, "dos", test2);
-
-	CU_basic_set_mode(CU_BRM_VERBOSE);
-	CU_basic_run_tests();
-	CU_cleanup_registry();
-
-	return CU_get_error();
-
-}
--- PROGRAMA/src/PROGRAMA.h
+++ PROGRAMA/src/PROGRAMA.h
@@ -29,22 +29,7 @@ int imprimirRespuesta(char *texto);
 void Error1(int code, char *err);
 //log
 t_log* logger ;
-// tests
-int correrTests();
+
 
 
 #endif /* PROGRAMA_H_ */
-#ifndef CUNIT_DEF_H_
-#define CUNIT_DEF_H_
-
-	#include <CUnit/Basic.h>
-
-	#ifndef DEFAULT_MAX_SUITES
-		#define DEFAULT_MAX_SUITES 100
-	#endif
-
-	int suite_index = 0;
-
-	CU_SuiteInfo suites[] = { [ 0 ... (DEFAULT_MAX_SUITES - 1)] = CU_SUITE_INFO_NULL};
-
-#endif /* CUNIT_DEF_H_ */
