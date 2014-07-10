36ec5eece9281174433105759c01b8b7d1c2cfac
vilarod <vilarod@gmail.com>
2014-07-10 19:29:16 -0300

    make con librerias

---
+++ make.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" &&pwd)"
+
+#MAKE CLEAN DE LOS PROYECTOS
+
+cd $DIR/Commons/Debug
+make clean
+
+cd $DIR/Parser/Debug
+make clean
+
+cd $DIR/UMV/Debug
+make clean
+
+cd $DIR/KERNEL/Debug
+make clean
+
+cd $DIR/PROGRAMA/Debug
+make clean
+
+cd $DIR/CPU/Debug
+make clean
+
+
+# MAKE DE LOS PROYECTOS
+
+cd $DIR/Commons/Debug
+make all
+make install
+
+cd $DIR/Parser/Debug
+make all
+make install
+
+cd $DIR/UMV/Debug
+make
+
+cd $DIR/KERNEL/Debug
+make
+
+cd $DIR/PROGRAMA/Debug
+make
+
+cd $DIR/CPU/Debug
+make
+
+
+# CREACION DISCO.BIN EN /home/utnso
+dd if=/dev/urandom of=/home/utnso/disco.bin bs=1024 count=102400
+
+# Se hace el export en el .bashrc
+echo >> /home/utnso/.bashrc
+echo export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$DIR/Commons/Debug >> /home/utnso/.bashrc
+echo export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$DIR/Parser/Debug >> /home/utnso/.bashrc
+
+#Se hace EL EXPORT DE LINKEO DE LA COMMONS EN CADA PROYECTO QUE LA USE
+export LD_LIBRARY_PATH=$DIR/Commons/Debug:$DIR/UMV/Debug
+export LD_LIBRARY_PATH=$DIR/Commons/Debug:$DIR/KERNEL/Debug
+export LD_LIBRARY_PATH=$DIR/Commons/Debug:$DIR/PROGRAMA/Debug
+export LD_LIBRARY_PATH=$DIR/Commons/Debug:$DIR/CPU/Debug
+export LD_LIBRARY_PATH=$DIR/Parser/Debug:$DIR/UMV/Debug
+export LD_LIBRARY_PATH=$DIR/Parser/Debug:$DIR/KERNEL/Debug
+export LD_LIBRARY_PATH=$DIR/Parser/Debug:$DIR/CPU/Debug
+
+#Crear link symbolic
+echo ln -s /home/utnso/tp-2014-1c-garras/PROGRAMA/Debug/PROGRAMA /usr/bin/ansisop
+
+cd $DIR
